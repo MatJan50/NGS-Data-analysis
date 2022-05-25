4131ce3592e65f6e48b4ab05d567c71624500c4f
MatJan50 <mateusz.janusz50@gmail.com>
2022-05-25 17:01:10 +0200

    ngs_analysis

--- rsc/variant_calling.sh
+++ rsc/variant_calling.sh
@@ -3,6 +3,6 @@ mkdir ../vcfs/
 for (( i = 5; i <= 7; i++ ))
         do
         echo "Variant calling..." 
-        bcftools mpileup -Ou -f ../saccharomyces_reference_genome.fna.gz "../mapping/SRR06454${i}.sorted.bam" | bcftools call --ploidy 1 -mv -Ob | bcftools view -i '%QUAL>=20' > "../vcfs/SRR06454$i.bcf"
+        bcftools mpileup -Ou -f ../mapping/saccharomyces_reference_genome.fna.gz "../mapping/SRR06454${i}.sorted.bam" | bcftools call --ploidy 1 -mv -Ob | bcftools view -i '%QUAL>=20' > "../vcfs/SRR06454$i.bcf"
 done
 
