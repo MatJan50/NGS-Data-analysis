1bb7afc5696181f01be848bda88909aee72502c0
MatJan50 <100695280+MatJan50@users.noreply.github.com>
2022-05-26 11:59:47 +0200

    Delete rsc directory

--- rsc/download_fastq.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd ../original_files/
-for (( i = 5; i <= 7; i++ ))
-	do
-	echo "Hold on..."  
-	fastq-dump -X 1000000 "SRR06454$i" --split-3 --gzip
-done
-echo Done 
--- rsc/variant_calling.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir ../vcfs/
-for (( i = 5; i <= 7; i++ ))
-        do
-        echo "Variant calling..." 
-        bcftools mpileup -Ou -f ../mapping/saccharomyces_reference_genome.fna.gz "../mapping/SRR06454${i}.sorted.bam" | bcftools call --ploidy 1 -mv -Ob | bcftools view -i '%QUAL>=20' > "../vcfs/SRR06454$i.bcf"
-done
-
