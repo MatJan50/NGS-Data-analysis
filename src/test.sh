8bdd05d9d0def7fe897f17f20fb3679819568b2f
MatJan50 <100695280+MatJan50@users.noreply.github.com>
2022-05-26 12:00:15 +0200

    scripts_ngs

---
+++ src/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd ../mapping/
+bwa index saccharomyces_reference_genome.fna.gz
+for (( i = 5; i <= 7; i++ ))
+        do
+        echo "Indexing..." 
+        bwa mem -t 4 saccharomyces_reference_genome.fna.gz "../quality_control/SRR06454${i}_1.fastp.fastq.gz" "../quality_control/SRR06454${i}_2.fastp.fastq.gz" > "../mapping/SRR06454${i}.sam"
+	samtools view -Sb "../mapping/SRR06454${i}.sam" > "../mapping/SRR06454${i}.bam"
+
+done
+
